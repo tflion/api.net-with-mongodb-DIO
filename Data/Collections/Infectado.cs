194813f45a500b27789a7de3ee35de01d9b5b98e
tflion <tfleao07@gmail.com>
2021-06-02 13:56:05 -0300

    projeto-api-mongo-dio

---
+++ Data/Collections/Infectado.cs
@@ -0,0 +1,19 @@
+using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+
+namespace API_MONGO_DIO.Data.Collections
+{
+    public class Infectado
+    {
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+        
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+    }
+}
